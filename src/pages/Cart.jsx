4135ea09ee4c737b5a62dbe30593bf88d9229098
Pranita <pfulsundar8@gmail.com>
2023-04-23 18:53:44 +0530

    fix: indentation

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,23 +8,23 @@ const Cart = () =>{
     return(
         <>
             { 
-                         cart.length === 0 ? 
-                            (<h1>Go to Menu page!</h1>) : 
-                                (
-                                    <>
-                                    <h1>Cart</h1>
-                                    <h2>Total Delivery Time: {totalDeliveryTime} minutes</h2>
-                                    <h2>Total Price: Rs {totalPrice}</h2>
-                                    <button onClick={()=>handleDiscountBtn()}>Apply Coupon</button>
-                                    <div className='all-menu'>
-                                        {
-                                            cart.map((item)=>(
-                                                <MenuCard key={item.id}  item={item} addedInCart/>
-                                            ))
-                                        }
-                                    </div>
-                                    </>
-                                )
+                cart.length === 0 ? 
+                    (<h1>Go to Menu page!</h1>) : 
+                        (
+                            <>
+                                <h1>Cart</h1>
+                                <h2>Total Delivery Time: {totalDeliveryTime} minutes</h2>
+                                <h2>Total Price: Rs {totalPrice}</h2>
+                                <button onClick={()=>handleDiscountBtn()}>Apply Coupon</button>
+                                <div className='all-menu'>
+                                    {
+                                        cart.map((item)=>(
+                                            <MenuCard key={item.id}  item={item} addedInCart/>
+                                        ))
+                                    }
+                                </div>
+                            </>
+                        )
             }           
         </>
     )
